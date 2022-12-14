cfd9c45fa6683662b2c93cf2c8b0583818fc03fc
ogryaznov <gryaznovoleg2002@gmail.com>
2022-12-15 00:50:05 +0200

    Added mongoose package

--- api/server.js
+++ api/server.js
@@ -10,25 +10,25 @@ app.use(express.static(__dirname + "/public"));
 
 
 app.get("/api/users", function(req, res){
-	const collection = req.app.locals.collection;
-	collection.find({}).toArray(function(err, users){
+	
+	User.find({}, function(err, users){
 		
 		if(err) return console.log(err);
-		res.send(users)
+		res.send(users);
 	});
 });
+
 app.get("/api/users/:id", function(req, res){
 	
-	const id = new objectId(req.params.id);
-	const collection = req.app.locals.collection;
-	collection.findOne({_id: id}, function(err, user){
+	const id = req.params.id;
+	User.findOne({_id: id}, function(err, user){
 		
 		if(err) return console.log(err);
 		res.send(user);
 	});
 });
 
-app.post("/api/users", jsonParser, function (req, res) {
+app.post("/api/users", jsonParser, function (req, res){
 	
 	if(!req.body) return res.sendStatus(400);
 	
@@ -36,11 +36,9 @@ app.post("/api/users", jsonParser, function (req, res) {
 	const userAge = req.body.age;
 	const userHeight = req.body.height;
 	const userWeight = req.body.weight;
-	const user = {name: userName, age: userAge, height: userHeight, weight: userWeight};
+	const user = new User({name: userName, age: userAge, height: userHeight, weight: userWeight});
 	
-	const collection = req.app.locals.collection;
-	collection.insertOne(user, function(err, result){
-		
+	user.save(function(err){
 		if(err) return console.log(err);
 		res.send(user);
 	});
@@ -48,12 +46,10 @@ app.post("/api/users", jsonParser, function (req, res) {
 
 app.delete("/api/users/:id", function(req, res){
 	
-	const id = new objectId(req.params.id);
-	const collection = req.app.locals.collection;
-	collection.findOneAndDelete({_id: id}, function(err, result){
+	const id = req.params.id;
+	User.findByIdAndDelete(id, function(err, user){
 		
 		if(err) return console.log(err);
-		let user = result.value;
 		res.send(user);
 	});
 });
@@ -61,19 +57,16 @@ app.delete("/api/users/:id", function(req, res){
 app.put("/api/users", jsonParser, function(req, res){
 	
 	if(!req.body) return res.sendStatus(400);
-	const id = new objectId(req.body.id);
+	const id = req.body.id;
 	const userName = req.body.name;
 	const userAge = req.body.age;
 	const userHeight = req.body.height;
 	const userWeight = req.body.weight;
-	
-	const collection = req.app.locals.collection;
-	collection.findOneAndUpdate({_id: id}, { $set: {age: userAge, name: userName, height: userHeight, weight: userWeight}},
-		{returnOriginal: false}, function(err, result){
-			if(err) return console.log(err);
-			const user = result.value;
-			res.send(user);
-		});
+	const newUser = {name: userName, age: userAge, height: userHeight, weight: userWeight};
+	User.findOneAndUpdate({_id: id}, newUser, {new: true}, function(err, user){
+		if(err) return console.log(err);
+		res.send(user);
+	});
 });
 
 module.exports = app;
